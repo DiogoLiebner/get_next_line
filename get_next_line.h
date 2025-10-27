38c8eb6903e10745e2ee378ee1a52ca0cfdb15f3
Diogo Lima Liebner <dlima-li@c1r5s1.42porto.com>
2025-10-27 15:27:00 +0000

    added necessary files for project to start

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlima-li <dlima-li@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 13:23:52 by dlima-li          #+#    #+#             */
+/*   Updated: 2025/10/27 13:51:57 by dlima-li         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char    *get_next_line(int fd);
